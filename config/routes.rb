2fcf5f3216e7c235c6f55ee58be5f155e9935f18
Krishna Krishna <kvanst3@gmail.com>
2019-05-23 15:57:37 +0200

    half done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  # get 'doses/index'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/show'
+  # get 'doses/edit'
+  # get 'doses/update'
+  # get 'doses/destroy'
+  # get 'ingredients/index'
+  # get 'ingredients/new'
+  # get 'ingredients/create'
+  # get 'ingredients/show'
+  # get 'ingredients/edit'
+  # get 'ingredients/update'
+  # get 'ingredients/destroy'
+  # get 'cocktails/index'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+  # get 'cocktails/show'
+  # get 'cocktails/edit'
+  # get 'cocktails/update'
+  # get 'cocktails/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do
+    resources :doses
+  end
+  resources :ingredients
+  resources :doses, only: :destroy
 end
